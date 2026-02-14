543c254101f666a8998f6daf331ed47d0e2d774a
LORD PREMO <cypherx599@gmail.com>
2026-02-14 21:00:14 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+export const BOT_NAME = 'Broken Lord';
+export const OWNER_NAME = 'Broken';
+export const PREFIX = '.';
+export const TIMEZONE = 'Africa/Dar_es_Salaam';
+export const OWNER_NUMBER = '255773001107'; // badilisha kwa namba yako bila +
+export const AI_API = 'https://ai-api-broken.vercel.app';
+export const MEDIA_API = 'https://broken-api-video-emage-hub.vercel.app';
